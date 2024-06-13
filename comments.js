467b40dec2f7486e12820ca2767792bd03bb0218
anav pdel <smita.732411@thc.tu.edu.np>
2024-06-13 06:27:34 +0000

    Comment copilot done

--- comments.js
+++ comments.js
@@ -25,7 +25,4 @@ app.get('/api/comments', function(req, res) {
   res.json(comments);
 });
 
-//add comments
-app.post('/api/comments', function(req, res) {
-  var comment = {
-    id: Date.now(),
+
